d4a7ab66415dd546f0c2b0052e85ff6d5a0a5b2c
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-03-19 18:22:22 -0400

    Full screen button increases height

--- test.js
+++ test.js
@@ -39,8 +39,26 @@ function minimizeWindow(windowName) {
   if (window.style.height != "0px") {
     window.style.height = "0px";
     content.style.display = "none";
+    window.style.paddingBottom = "";
   } else {
     window.style.height = ""; // Default value
     content.style.display = ""; // Also default
+    window.style.paddingBottom = "";
+  }
+}
+
+// Maximinize window on click
+function maximizeWindow(windowName) {
+  
+  var window = document.getElementById(windowName);
+  var content = window.querySelector(".content.active");
+
+  if (window.style.height === "0px") { // If minimized, make the content visible again
+    window.style.height = "";
+    content.style.display = "";
+  } else if (window.style.paddingBottom != "50px") {
+    window.style.paddingBottom = "50px";
+  } else {
+    window.style.paddingBottom = "0px";
   }
 }
