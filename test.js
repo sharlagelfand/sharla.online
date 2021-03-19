042ba7f76deb155d19b18aed6a2f342c9a0284a4
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-03-19 14:16:54 -0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,46 @@
+// Adding time in
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+
+// Tabbing:
+// Source: https://www.w3schools.com/howto/howto_js_tabs.asp
+function openTab(evt, tabName) {
+  
+  // Get all elements with class="tabcontent" and hide them, also remove the active class
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  for (var i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    tabcontent[i].className = tabcontent[i].className.replace(" active", "");
+  }
+  // Get all elements with class="tablink" and remove the class "active"
+  var tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  // Show the current tab, add an "active" class to the button that opened the tab, and to the tab itself
+  document.getElementById(tabName).style.display = "inline";
+  document.getElementById(tabName).className += " active";
+  evt.currentTarget.className += " active";
+  
+  // If the window is minimized, open it
+  var parentDivId = document.getElementById(tabName).parentElement.id;
+  var parentDiv = document.getElementById(parentDivId);
+  if (parentDiv.style.height === "0px") {
+    parentDiv.style.height = "";
+  }
+}
+
+// Minimize window on click
+function minimizeWindow(windowName) {
+  
+  var window = document.getElementById(windowName);
+  var content = window.querySelector(".content.active");
+  
+  if (window.style.height != "0px") {
+    window.style.height = "0px";
+    content.style.display = "none";
+  } else {
+    window.style.height = ""; // Default value
+    content.style.display = ""; // Also default
+  }
+}
