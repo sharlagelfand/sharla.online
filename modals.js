b63f81989c61ea5eab4a3ea603d4494faf92cdb3
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-12-21 20:06:30 -0500

    Add modal triggers for 2021

--- modals.js
+++ modals.js
@@ -9,9 +9,11 @@ function closeModal(modalName) {
 }
 
 window.onclick = function(event) {
+  var updateModal = document.getElementById("2021-update-modal")
   var talksModal = document.getElementById("talks-modal")
   var otherProjectsModal = document.getElementById("other-projects-modal")
   var keyboardModal = document.getElementById("keyboard-modal")
+  var updateModal = document.getElementById("2021-update-modal")
 
   if (event.target == talksModal) {
     talksModal.style.display = "none";
@@ -19,11 +21,14 @@ window.onclick = function(event) {
     otherProjectsModal.style.display = "none";
    } else if (event.target == keyboardModal){
      keyboardModal.style.display = "none";
-   }
+   } else if (event.target == keyboardModal){
+     updateModal.style.display = "none";
+  }
 }
 
 document.addEventListener('keydown', (event) => {
 
+  var updateModal = document.getElementById("2021-update-modal")
   var talksModal = document.getElementById("talks-modal")
   var otherProjectsModal = document.getElementById("other-projects-modal")
   var keyboardModal = document.getElementById("keyboard-modal")
@@ -32,5 +37,6 @@ document.addEventListener('keydown', (event) => {
     otherProjectsModal.style.display = "none";
     talksModal.style.display = "none";
     keyboardModal.style.display = "none";
+    updateModal.style.display = "none";
   }
 })
